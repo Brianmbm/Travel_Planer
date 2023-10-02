df122505ba1f25f1cca4ac9bb3c5434f547807b8
Brian Bocquet <brianbocquet@gmail.com>
2023-10-02 14:00:13 +0200

    Added test buttons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,13 +1,25 @@
-﻿namespace Travel_Planer
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+
+namespace Travel_Planer
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            map.Pins.Add(new Pin()
+            {
+                Location = new Location(50,6),
+                Label = "doibndbvnd",
+                Address = "svsvpsnv"
+            });
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(new Location (50, 6), Distance.FromKilometers(10)));
+        }
     }
 }
